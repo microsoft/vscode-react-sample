435c061d5c25dab7745bc20c5d9a57994fd4fd8d
Wade Anderson <waander@Wades-MacBook-Pro.local>
2016-06-08 10:49:04 -0700

    Some spacing added.

--- server/server.js
+++ server/server.js
@@ -3,26 +3,40 @@ var bodyParser = require('body-parser')
 
 var database = require('./database');
 
-// Create server
 var server = express();
 server.use(bodyParser.json());
 
-// Middleware
+
+
+
+
+
+
+
 server.use(function (req, res, next) {
-  // allow origin for demo purposes
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
-// Routes
+
+
+
+
+
+
 server.get('/todos', function(req, res, next) {
   database.getAll(function(todos) {
     res.send(todos);
     next();
   });
 });
+
+
+
+
 
 server.post('/todos', function(req, res, next) {
   var todo = req.body;
@@ -41,8 +55,11 @@ server.delete('/todos/:id', function(req, res, next) {
   });
 });
 
-// Start listening
-var PORT = 3001;
+
+
+
+
+var PORT = 3002;
 server.listen(PORT, function() {
   console.log('listening at %s', PORT);
 });

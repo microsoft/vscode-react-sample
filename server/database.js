b32315eb038af4b9fc2ba574255b43cb38d4651b
Wade Anderson <waander@Wades-MacBook-Pro.local>
2016-07-20 11:12:41 -0700

    Changes to database.js

--- server/database.js
+++ server/database.js
@@ -5,13 +5,14 @@ var DATA = 'data/todos.json';
 var PRETTIFY_WS = 4;
 
 
+
 function getAll(resolve) {
     fs.readFile(DATA, function(err, data) {
       resolve(JSON.parse(data));
     });
 }
 
-function commit(data, resolve) {
+function commitData(data, resolve) {
     fs.writeFile(DATA, JSON.stringify(data, null, PRETTIFY_WS));
 }
 

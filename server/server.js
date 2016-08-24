384bf6a005e087a4a791d81ec00e9850a4b3e932
Wade Anderson <waander@Wades-MacBook-Pro.local>
2016-08-24 11:57:46 -0700

    Removed some smallcomments.

--- server/server.js
+++ server/server.js
@@ -4,11 +4,9 @@ var _ = require('lodash');
 
 var database = require('./database');
 
-// Create server
 var server = express();
 server.use(bodyParser.json());
 
-// Middleware
 server.use(function (req, res, next) {
   // allow origin for demo purposes
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
